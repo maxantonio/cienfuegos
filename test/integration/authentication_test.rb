50579a533da24c63daf6b56866e416e0a560c34d
Javier Cadiz <cadizjavier@gmail.com>
2014-07-23 22:28:09 -0400

    Create Integration test for authentication workflow.

---
+++ test/integration/authentication_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class AuthenticationTest < ActionDispatch::IntegrationTest
+
+  test "sign in an existing user" do
+    visit sign_in_path
+
+    fill_in "Email",    with: "tim@apple.com"
+    fill_in "Password", with: "foobar"
+
+    click_button "Sign in"
+  end
+
+  test "sign up a new user" do
+    visit sign_up_path
+
+    fill_in "Name",     with: "John Doe"
+    fill_in "Email",    with: "john@apple.com"
+    fill_in "Password", with: "foobar"
+
+    click_button "Create my account"
+  end
+
+end
+
+
