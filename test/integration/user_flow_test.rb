9887940a8c17d69a9e6d72e29755e7f8f97b9841
Javier Cadiz <cadizjavier@gmail.com>
2014-07-25 14:15:56 -0400

    Add a bunch of integration test to check the user flow.

---
+++ test/integration/user_flow_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UserFlowTest < ActionDispatch::IntegrationTest
+
+  test "when users can create a post and others can't see it" do
+    visit sign_in_path
+
+    fill_in "Email", with: "tim@apple.com"
+    fill_in "Password", with: "foobar"
+
+    click_button "Sign in"
+
+    click_link "New Post"
+
+    fill_in "Title", with: "Apple announcement"
+    fill_in "Body", with: "Our company is way ahead of the competition
+                           in terms of customer satisfaction."
+
+    click_button "Update Post"
+
+    visit posts_path
+
+    assert page.has_content?('Apple announcement'), 'The Apple announcement does not appears.'
+
+    # Log in as another user and check if the post is visible
+
+    click_link 'Sign out'
+
+    visit sign_in_path
+    fill_in "Email", with: "john@apple.com"
+    fill_in "Password", with: "foobar"
+    click_button 'Sign in'
+
+    assert_not page.has_content?('Apple announcement'), 'The Apple announcement belongs to another user.'
+
+  end
+
+end
+
